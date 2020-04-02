b611c1050d0bfeea1e9a664c0553d299f8267f85
Swapnil Bawaskar <sbawaskar@pivotal.io>
2020-04-02 09:34:56 -0700

    initial riff plugin hack

--- cmd/octant-sample-plugin/main.go
+++ cmd/octant-sample-plugin/main.go
@@ -1,16 +1,14 @@
-/*
-Copyright (c) 2019 the Octant contributors. All Rights Reserved.
-SPDX-License-Identifier: Apache-2.0
-*/
-
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"sort"
 	"time"
 
 	"github.com/pkg/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
 	"github.com/vmware-tanzu/octant/pkg/navigation"
@@ -21,7 +19,7 @@ import (
 	"github.com/vmware-tanzu/octant/pkg/view/flexlayout"
 )
 
-var pluginName = "plugin-name"
+var pluginName = "octant-riff"
 
 // This is a sample plugin showing the features of Octant's plugin API.
 func main() {
@@ -29,12 +27,12 @@ func main() {
 	log.SetPrefix("")
 
 	// This plugin is interested in Pods
-	podGVK := schema.GroupVersionKind{Version: "v1", Kind: "Pod"}
+	functionGVK := schema.GroupVersionKind{Group: "build.projectriff.io", Version: "v1alpha1", Kind: "Function"}
 
 	// Tell Octant to call this plugin when printing configuration or tabs for Pods
 	capabilities := &plugin.Capabilities{
-		SupportsPrinterConfig: []schema.GroupVersionKind{podGVK},
-		SupportsTab:           []schema.GroupVersionKind{podGVK},
+		SupportsPrinterConfig: []schema.GroupVersionKind{functionGVK},
+		SupportsTab:           []schema.GroupVersionKind{functionGVK},
 		IsModule:              true,
 	}
 
@@ -46,18 +44,20 @@ func main() {
 	}
 
 	// Use the plugin service helper to register this plugin.
-	p, err := service.Register(pluginName, "a description", capabilities, options...)
+	p, err := service.Register(pluginName, "visualize riff", capabilities, options...)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// The plugin can log and the log messages will show up in Octant.
-	log.Printf("octant-sample-plugin is starting")
+	log.Printf("octant-riff plugin is starting")
+	//fmt.Println("octant-riff plugin is starting")
 	p.Serve()
 }
 
 // handleTab is called when Octant wants to print a tab for an object.
 func handleTab(request *service.PrintRequest) (plugin.TabResponse, error) {
+	log.Println("SWAP:handleTab")
 	if request.Object == nil {
 		return plugin.TabResponse{}, errors.New("object is nil")
 	}
@@ -82,13 +82,14 @@ func handleTab(request *service.PrintRequest) (plugin.TabResponse, error) {
 
 	// In this example, this plugin will tell Octant to create a new
 	// tab when showing pods. This tab's name will be "Extra Pod Details".
-	tab := component.NewTabWithContents(*layout.ToComponent("Extra Pod Details"))
+	tab := component.NewTabWithContents(*layout.ToComponent("Function Details"))
 
 	return plugin.TabResponse{Tab: tab}, nil
 }
 
 // handlePrint is called when Octant wants to print an object.
 func handlePrint(request *service.PrintRequest) (plugin.PrintResponse, error) {
+	log.Println("SWAP:handlePrint")
 	if request.Object == nil {
 		return plugin.PrintResponse{}, errors.Errorf("object is nil")
 	}
@@ -113,8 +114,12 @@ func handlePrint(request *service.PrintRequest) (plugin.PrintResponse, error) {
 
 	// Octant has a component library that can be used to build content for a plugin.
 	// In this case, the plugin is creating a card.
-	podCard := component.NewCard(component.TitleFromString(fmt.Sprintf("Extra Output for %s", u.GetName())))
-	podCard.SetBody(component.NewMarkdownText("This output was generated from _octant-sample-plugin_"))
+	podCard := component.NewCard(component.TitleFromString(fmt.Sprintf("List of processors for %s", u.GetName())))
+	processorList, err := getProcessorsForFunction(request.DashboardClient, u)
+	if err != nil {
+		return plugin.PrintResponse{}, nil
+	}
+	podCard.SetBody(component.NewMarkdownText(processorList))
 
 	msg := fmt.Sprintf("update from plugin at %s", time.Now().Format(time.RFC3339))
 
@@ -141,28 +146,45 @@ func handlePrint(request *service.PrintRequest) (plugin.PrintResponse, error) {
 	}, nil
 }
 
+func getFunctions(client service.Dashboard) []string {
+	functionGVK := schema.GroupVersionKind{Group: "build.projectriff.io", Version: "v1alpha1", Kind: "Function"}
+	functionKey := store.KeyFromGroupVersionKind(functionGVK)
+	result := []string{}
+
+	l, err := client.List(context.Background(), functionKey)
+	if err != nil {
+		panic(err)
+		//return append(result, component.NewText(fmt.Sprintf("%s", err)))
+	}
+
+	for _, i := range l.Items {
+		result = append(result, i.GetName())
+	}
+	//for _, i := range l.Items {
+	//	result = append(result, component.NewLink(i.GetName(), "",
+	//		fmt.Sprintf("/custom-resources/functions.build.projectriff.io/v1alpha1/%s", i.GetName())))
+	//}
+	return result
+}
+
+func getProcessorsForFunction(client service.Dashboard, function *unstructured.Unstructured) (string, error) {
+	//processorGVK := schema.GroupVersionKind{Group: "streaming.projectriff.io", Version: "v1alpha1", Kind: "Processor"}
+	//processorKey := store.KeyFromGroupVersionKind(processorGVK)
+	//l, err := client.List(context.Background(), processorKey)
+	//if err != nil {
+	//	return "", err
+	//}
+	return "WORK IN PROGRESS", nil
+}
+
 // handlePrint creates a navigation tree for this plugin. Navigation is dynamic and will
 // be called frequently from Octant. Navigation is a tree of `Navigation` structs.
 // The plugin can use whatever paths it likes since these paths can be namespaced to the
 // the plugin.
 func handleNavigation(request *service.NavigationRequest) (navigation.Navigation, error) {
 	return navigation.Navigation{
-		Title: "Sample Plugin",
+		Title: "riff Plugin",
 		Path:  request.GeneratePath(),
-		Children: []navigation.Navigation{
-			{
-				Title:    "Nested Once",
-				Path:     request.GeneratePath("nested-once"),
-				IconName: "folder",
-				Children: []navigation.Navigation{
-					{
-						Title:    "Nested Twice",
-						Path:     request.GeneratePath("nested-once", "nested-twice"),
-						IconName: "folder",
-					},
-				},
-			},
-		},
 		IconName: "cloud",
 	}, nil
 }
@@ -170,25 +192,80 @@ func handleNavigation(request *service.NavigationRequest) (navigation.Navigation
 // initRoutes routes for this plugin. In this example, there is a global catch all route
 // that will return the content for every single path.
 func initRoutes(router *service.Router) {
-	gen := func(name, accessor, requestPath string) component.Component {
-		cardBody := component.NewText(fmt.Sprintf("hello from plugin: path %s", requestPath))
-		card := component.NewCard(component.TitleFromString(fmt.Sprintf("My Card - %s", name)))
-		card.SetBody(cardBody)
-		cardList := component.NewCardList(name)
-		cardList.AddCard(*card)
-		cardList.SetAccessor(accessor)
-
-		return cardList
-	}
 
 	router.HandleFunc("*", func(request service.Request) (component.ContentResponse, error) {
-		// For each page, generate two tabs with a some content.
-		component1 := gen("Tab 1", "tab1", request.Path())
-		component2 := gen("Tab 2", "tab2", request.Path())
 
-		contentResponse := component.NewContentResponse(component.TitleFromString("Example"))
-		contentResponse.Add(component1, component2)
+		card := component.NewCard([]component.TitleComponent{component.NewText("functions")})
+
+		processorGVK := schema.GroupVersionKind{Group: "streaming.projectriff.io", Version: "v1alpha1", Kind: "Processor"}
+		processorKey := store.KeyFromGroupVersionKind(processorGVK)
+		l, err := request.DashboardClient().List(context.Background(), processorKey)
+		if err != nil {
+			return component.ContentResponse{}, err
+		}
+		functions := map[string]bool{}
+		table := component.NewTable("Functions", "placeholder",
+			[]component.TableCol{
+				component.TableCol{Name: "function"},
+				component.TableCol{Name: "processor"},
+				component.TableCol{Name: "input streams"},
+				component.TableCol{Name: "output streams"},
+			})
+
+		component.new
+		sortedList := sortUnstructuredList(l.Items)
+		for _, i := range sortedList {
+			funcName, found, err := unstructured.NestedString(i.UnstructuredContent(), "spec", "build", "functionRef")
+			if err != nil || !found {
+				funcName = "NOT FOUND"
+			}
+			inStreamStr := getStreamNames(i, "spec", "inputs")
+			outStreamStr := getStreamNames(i, "spec", "outputs")
+			table.Add(component.TableRow{
+				"function":      component.NewText(funcName),
+				"processor":     component.NewText(i.GetName()),
+				"input streams": component.NewText(fmt.Sprintf("%s", inStreamStr)),
+				"output streams": component.NewText(fmt.Sprintf("%s", outStreamStr)),
+			})
+			functions[funcName] = true
+		}
+
+		allFunctions := getFunctions(request.DashboardClient())
+		for fi := range allFunctions {
+			if _, ok := functions[allFunctions[fi]]; !ok {
+				table.Add(component.TableRow{
+					"function": component.NewText(allFunctions[fi]),
+				})
+			}
+		}
+
+		card.SetBody(table)
+		contentResponse := component.NewContentResponse(component.TitleFromString("riff Components"))
+		contentResponse.Add(card)
 
 		return *contentResponse, nil
 	})
+}
+
+func sortUnstructuredList(items []unstructured.Unstructured) []unstructured.Unstructured {
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].GetName() < items[j].GetName()
+	})
+	return items
+}
+
+func getStreamNames(i unstructured.Unstructured, fields ...string) []string {
+	inStreamStr := []string{}
+	inStreams, found, err := unstructured.NestedSlice(i.UnstructuredContent(), fields...)
+	if err != nil {
+		return append(inStreamStr, fmt.Sprintf("%s", err))
+	}
+	if !found {
+		return append(inStreamStr, "")
+	}
+	for i := range inStreams {
+		stream := inStreams[i].(map[string]interface{})
+		inStreamStr = append(inStreamStr, fmt.Sprintf("%s", stream["stream"]))
+	}
+	return inStreamStr
 }
